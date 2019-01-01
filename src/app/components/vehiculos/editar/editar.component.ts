2675d5cb3fa8302e1b69d71a11b94ffc185862a3
rulo4 <nebel04@gmail.com>
2019-01-01 10:52:25 -0600

    Se implementa componente para la actualización de vehículos

--- src/app/components/vehiculos/editar/editar.component.ts
+++ src/app/components/vehiculos/editar/editar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Vehiculo } from '../inicio/Vehiculo';
+import { VehiculosService } from '../../../vehiculos.service';
 
 @Component({
   selector: 'app-editar',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarComponent implements OnInit {
 
-  constructor() { }
+  vehiculo: any = {};
+  ngForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private vehiculosService: VehiculosService,
+    private formBuilder: FormBuilder) {
+    this.crearForm();
+  }
+  crearForm() {
+    this.ngForm = this.formBuilder.group({
+      tipo: [ '', Validators.required ],
+      placas: [ '', Validators.required ],
+      soatfec: [ '', Validators.required ],
+      serviciofec: ['', Validators.required]
+    });
+  }
+
+  actualizarVehiculo(vehiculo) {
+    this.route.params.subscribe(params => {
+      this.vehiculosService.actualizarVehiculo(vehiculo).subscribe(response => {
+        this.router.navigate([ '/vehiculos' ]);
+      });
+    });
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.vehiculosService.editarVehiculo(params[ 'id' ]).subscribe(response => {
+        this.vehiculo = response;
+        this.ngForm.get('tipo').setValue(this.vehiculo.tipo);
+        this.ngForm.get('placas').setValue(this.vehiculo.placas);
+        this.ngForm.get('soatfec').setValue(this.vehiculo.soatfec);
+        this.ngForm.get('serviciofec').setValue(this.vehiculo.serviciofec);
+      });
+    });
   }
 
 }

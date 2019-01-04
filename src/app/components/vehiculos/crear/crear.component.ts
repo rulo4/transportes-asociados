7ae9e728e1d2927f76b14759a03dc54ccc26de77
rulo4 <nebel04@gmail.com>
2019-01-04 00:45:43 -0600

    Se incorpora el uso de toasts y se incorporan date pickers

--- src/app/components/vehiculos/crear/crear.component.ts
+++ src/app/components/vehiculos/crear/crear.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as moment from 'moment-timezone';
+import { ToastrService } from 'ngx-toastr';
 import { VehiculosService } from '../../../vehiculos.service';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-crear',
@@ -8,10 +11,9 @@ import { VehiculosService } from '../../../vehiculos.service';
   styleUrls: ['./crear.component.css']
 })
 export class CrearComponent implements OnInit {
-
   ngForm: FormGroup;
 
-  constructor(private vehiculoService: VehiculosService, private formBuilder: FormBuilder) {
+  constructor(private vehiculoService: VehiculosService, private formBuilder: FormBuilder, private toastr: ToastrService) {
     this.crearForm();
   }
 
@@ -24,8 +26,24 @@ export class CrearComponent implements OnInit {
     });
   }
 
-  crearVehiculo(vehiculo) {
-    this.vehiculoService.crearVehiculo(vehiculo);
+  crearVehiculo() {
+    const vehiculo = {
+      tipo: this.ngForm.get('tipo').value,
+      placas: this.ngForm.get('placas').value,
+      soatfec: this.aMoment(this.ngForm.get('soatfec').value),
+      serviciofec: this.aMoment(this.ngForm.get('serviciofec').value)
+    };
+    this.vehiculoService.crearVehiculo(vehiculo).subscribe(response => {
+      if (response['msj']) {
+        this.toastr.success(response['msj']);
+      } else if (response['err']) {
+        this.toastr.error(response['err']);
+      }
+    });
+  }
+
+  aMoment(fecha: NgbDateStruct) {
+    return moment.tz({year: fecha.year, month: fecha.month - 1, day: fecha.day}, 'America/Mexico_City');
   }
 
   ngOnInit() {

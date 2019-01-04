b54604c5e40a3090fb5b82faa30512d67a21b669
rulo4 <nebel04@gmail.com>
2019-01-04 00:48:54 -0600

    Se incorporan date pickers y manipulación de fechas para mostrar y enviar fechas en los formatos requeridos

--- src/app/components/vehiculos/editar/editar.component.ts
+++ src/app/components/vehiculos/editar/editar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as moment from 'moment-timezone';
 
 import { Vehiculo } from '../inicio/Vehiculo';
 import { VehiculosService } from '../../../vehiculos.service';
+import { NgbDateStruct, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-editar',
@@ -12,6 +14,8 @@ import { VehiculosService } from '../../../vehiculos.service';
 })
 export class EditarComponent implements OnInit {
 
+  soatfec: NgbDateStruct;
+  serviciofec: NgbDateStruct;
   vehiculo: any = {};
   ngForm: FormGroup;
 
@@ -31,8 +35,32 @@ export class EditarComponent implements OnInit {
     });
   }
 
-  actualizarVehiculo(vehiculo) {
+  llenarCampos(vehiculo) {
     this.route.params.subscribe(params => {
+      this.vehiculosService.editarVehiculo(params[ 'id' ]).subscribe(response => {
+        this.vehiculo = response;
+        this.ngForm.get('tipo').setValue(this.vehiculo.tipo);
+        this.ngForm.get('placas').setValue(this.vehiculo.placas);
+        this.ngForm.get('soatfec').setValue(this.aNgbDate(this.vehiculo.soatfec));
+        this.ngForm.get('serviciofec').setValue(this.aNgbDate(this.vehiculo.serviciofec));
+      });
+    });
+  }
+
+  actualizarVehiculo() {
+    this.soatfec = this.ngForm.get('soatfec').value;
+    this.serviciofec = this.ngForm.get('serviciofec').value;
+    this.soatfec.month--;
+    this.serviciofec.month--;
+    const vehiculo = {
+      id: null,
+      tipo: this.ngForm.get('tipo').value,
+      placas: this.ngForm.get('placas').value,
+      soatfec: moment(this.soatfec),
+      serviciofec: moment(this.serviciofec)
+    };
+    this.route.params.subscribe(params => {
+      vehiculo.id = params.id;
       this.vehiculosService.actualizarVehiculo(vehiculo).subscribe(response => {
         this.router.navigate([ '/vehiculos' ]);
       });
@@ -40,15 +68,12 @@ export class EditarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.vehiculosService.editarVehiculo(params[ 'id' ]).subscribe(response => {
-        this.vehiculo = response;
-        this.ngForm.get('tipo').setValue(this.vehiculo.tipo);
-        this.ngForm.get('placas').setValue(this.vehiculo.placas);
-        this.ngForm.get('soatfec').setValue(this.vehiculo.soatfec);
-        this.ngForm.get('serviciofec').setValue(this.vehiculo.serviciofec);
-      });
-    });
+    this.llenarCampos(this.vehiculo);
+  }
+
+  aNgbDate(fecha: String) {
+    const momentfec = moment.utc(fecha);
+    return NgbDate.from(JSON.parse(momentfec.format('{["year"]: YYYY, ["month"]: M, ["day"]: D}')));
   }
 
 }

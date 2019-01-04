ea1432dcc10843daeacabdae236231148239e2e4
rulo4 <nebel04@gmail.com>
2019-01-04 00:43:45 -0600

    Se incorporó el uso de toasts, y se manipulan las fechas para ser mostradas con un formato más legible

--- src/app/components/vehiculos/inicio/inicio.component.ts
+++ src/app/components/vehiculos/inicio/inicio.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Vehiculo } from './Vehiculo';
+import * as moment from 'moment-timezone';
+import 'moment/min/locales';
+import { ToastrService } from 'ngx-toastr';
 import { VehiculosService } from '../../../vehiculos.service';
 
 @Component({
@@ -10,17 +12,30 @@ import { VehiculosService } from '../../../vehiculos.service';
 })
 export class InicioComponent implements OnInit {
 
-  vehiculos: Vehiculo[] = [];
+  vehiculos: any[] = [];
 
-  constructor(private router: Router, private vehiculosService: VehiculosService) { }
+  constructor(private router: Router, private vehiculosService: VehiculosService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    moment.locale('es');
     this.listarVehiculos();
   }
 
   listarVehiculos() {
-    this.vehiculosService.listarVehiculos().subscribe((vehiculos: Vehiculo[]) => {
-      this.vehiculos = vehiculos['vehiculos'];
+    this.vehiculosService.listarVehiculos().subscribe((vehiculos: any[]) => {
+      if (!vehiculos) {
+        this.toastr.error('Error al intentar obtener lista de vehículos');
+        return;
+      }
+      if (vehiculos.length !== 0) {
+        this.vehiculos = vehiculos.map((v) => {
+          v.soatfec = moment(v.soatfec).format('DD [de] MMM [de] YYYY');
+          v.serviciofec = moment(v.serviciofec).format('DD [de] MMM [de] YYYY');
+          return v;
+        });
+      } else {
+        this.toastr.warning('No hay vehículos para mostrar');
+      }
     });
   }
 
